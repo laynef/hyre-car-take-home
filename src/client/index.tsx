aa855e3656e7017f3e7f7e9cad9cce192fc79562
laynef <laynefaler@gmail.com>
2020-08-26 16:43:01 -0700

    Initial commit

---
+++ src/client/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.scss';
+import App from '../components/App';
+
+ReactDOM.hydrate(<App />, document.getElementById('root'));
