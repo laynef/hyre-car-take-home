1adf169848b2a3277556be5c805ed3fbfbbab3a7
laynef <laynefaler@gmail.com>
2020-08-26 20:21:19 -0700

    Added a router for application

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import NotFound from './NotFound';
 
 
 const uri = '/api/v1/graphql';
@@ -9,9 +12,12 @@ const client = new ApolloClient({ uri });
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <div className="w-100 h-100 bg-light">
-
-      </div>
+      <Router>
+        <div className="w-100 h-100 bg-light">
+          <Route path="/" component={HomePage} />
+          <Route path="*" component={NotFound} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
---
+++ src/client/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="w-100 h-100 bg-light">
+
+    </div>
+  );
+}
+
+export default HomePage;
