3612fea2aae5e3a89d3337017e52b03c9a1d32db
laynef <laynefaler@gmail.com>
2020-08-27 11:40:34 -0700

    Removed old query

--- src/client/components/HomePage.tsx
+++ src/client/components/HomePage.tsx
@@ -9,37 +9,10 @@ interface QueryChildren {
   data: any;
 }
 
-interface Results {
-  Make_ID: string;
-  Make_Name: string;
-}
-
-const MAKES_QUERY = gql`
-  query Makes {
-    makes {
-      Count
-      Results {
-        Make_ID
-        Make_Name
-      }
-    }
-  }
-`;
-
 const HomePage: React.FC = () => {
   return (
     <div className="w-100 h-100 bg-light pt-5">
         <h1 className="text-center font-weight-light pt-3">Hello World</h1>
-        <Query query={MAKES_QUERY}>
-          { 
-            ({ loading, data }: QueryChildren) => {
-              if (loading) return <p>Loading...</p>;
-              if (data && data.makes && data.makes.Results) {
-                return data.makes.Results.map((e: Results, i: number) => (<p key={i}>{e.Make_Name}</p>))
-              }
-            }
-          }
-        </Query>
     </div>
   );
 }
