e99193cbd456195f247f5b6d0d29b51216aa4e68
laynef <laynefaler@gmail.com>
2020-08-26 20:14:23 -0700

    Added apollo client

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
+
+const uri = '/api/v1/graphql';
+const client = new ApolloClient({ uri });
 
 const App: React.FC = () => {
   return (
-    <div className="w-100 h-100 bg-light">
+    <ApolloProvider client={client}>
+      <div className="w-100 h-100 bg-light">
 
-    </div>
+      </div>
+    </ApolloProvider>
   );
 }
 
